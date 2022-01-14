8cac22c68ec90aba012b418af21de66aa6a49ce0
Utsav Karan <ukaran11@gmail.com>
2022-01-14 23:00:08 +0530

    Utsav | Added beforeEach in Mainpage.test

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -1,6 +1,15 @@
 import { shallow, mount, render } from 'enzyme';
 import App from '../App';
 
+let wrapper;
+beforeEach(() => {
+    const mockProps = {
+        onRequestRobots : jest.fn(),
+        robots: [],
+        searchField: '',
+        isPending: false
+    }
+});
 
 test('expect to render App Component', () => {
     expect(shallow(<App />)).toMatchSnapshot();
