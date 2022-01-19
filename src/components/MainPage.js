97d344ae5be401adb1b563d15115d674594f82c0
Utsav Karan <ukaran11@gmail.com>
2022-01-19 23:44:04 +0530

    Utsav | added another test for MainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -16,8 +16,8 @@ class MainPage extends Component {
     this.props.onRequestRobots();
   }
 
-  filterRobots = robots => {
-    return robots.filter(robot => {
+  filterRobots = () => {
+    return this.props.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase());
     })
   }
@@ -32,7 +32,7 @@ class MainPage extends Component {
         <Scroll>
           { isPending ? <h1>Loading</h1> :
             <ErrorBoundry>
-              <CardList robots={this.filterRobots(robots)} />
+              <CardList robots={this.filterRobots()} />
             </ErrorBoundry>
           }
         </Scroll>
--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -1,5 +1,6 @@
 import { shallow, mount, render } from 'enzyme';
 import App from '../App';
+import MainPage from './MainPage';
 
 let wrapper;
 beforeEach(() => {
@@ -9,8 +10,60 @@ beforeEach(() => {
         searchField: '',
         isPending: false
     }
+    wrapper = shallow(<MainPage {...mockProps} />)
 });
 
 test('renders MainPage without crashing', () => {
-    expect(shallow(<App />)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('filters robots correctly', () => {
+    const mockProps2 = {
+        onRequestRobots : jest.fn(),
+        robots: [{
+            id: 3,
+            name: 'John',
+            email: 'john@gmail.com'
+        }],
+        searchField: 'john',
+        isPending: false
+    }
+    const wrapper2 = shallow(<MainPage {...mockProps2} />);
+    expect(wrapper2.instance().filterRobots()).toEqual([{
+        id: 3,
+        name: 'John',
+        email: 'john@gmail.com'
+    }]);
+})
+
+test('filters robots correctly 2', () => {
+    const mockProps3 = {
+        onRequestRobots : jest.fn(),
+        robots: [{
+            id: 3,
+            name: 'John',
+            email: 'john@gmail.com'
+        }],
+        searchField: 'a',
+        isPending: false
+    }
+    const filteredRobots = [];
+    const wrapper3 = shallow(<MainPage {...mockProps3} />);
+    expect(wrapper3.instance().filterRobots()).toEqual(filteredRobots);
+})
+
+test('filters robots correctly 2 with pending set to true', () => {
+    const mockProps3 = {
+        onRequestRobots : jest.fn(),
+        robots: [{
+            id: 3,
+            name: 'John',
+            email: 'john@gmail.com'
+        }],
+        searchField: 'a',
+        isPending: true
+    }
+    const filteredRobots = [];
+    const wrapper3 = shallow(<MainPage {...mockProps3} />);
+    expect(wrapper3.instance().filterRobots()).toEqual(filteredRobots);
 })
